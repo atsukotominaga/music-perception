d8e15fd2855f2eb457496220b6506605650b9a71
Atsuko Tominaga <Tominaga_Atsuko@phd.ceu.edu>
2021-08-26 17:04:54 +0200

    update analysis (with the 20th participant)
    Signed-off-by: Atsuko Tominaga <Tominaga_Atsuko@phd.ceu.edu>

--- analysis/questionnaire.R
+++ analysis/questionnaire.R
@@ -16,7 +16,7 @@ colnames(dt)[4] <- "Gender"
 colnames(dt)[7] <- "Handedness"
 
 # extract the total number of years of training for the longest instrument (input manually based on the original csv file)
-dt$Training <- c(14, 13, 7, 18, 23, 12, 17, 20, 12, 7, 12, 7, 7, 13, 20, 15, 6)
+dt$Training <- c(14, 13, 7, 18, 23, 12, 17, 20, 12, 7, 12, 7, 7, 13, 20, 15, 6, 15, 8, 5, 20)
 
 # exclude participants
 dt_included <- dt[SubNr != 4]
